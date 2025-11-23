7f883485edf8538a67e8ed7fdccba46aea142ff2
the-baclib-initiative <baclib@mailbox.org>
2025-11-23 11:49:32 +0100

    Add basic project source

---
+++ src/BacnetLibrary/BacnetLibrary.cs
@@ -0,0 +1,12 @@
+namespace BacnetLibrary;
+
+/// <summary>
+/// Main entry point and version information for the BACnet Library.
+/// </summary>
+public static class BacnetLibrary
+{
+    /// <summary>
+    /// Gets the library version.
+    /// </summary>
+    public static string Version => "0.1.0";
+}
